3709042e016640f34e5b809411dd3275d12e7ccf
ungden <tduong297@gmail.com>
2026-04-27 02:53:29 +0700

    config: whitelist randomuser.me for next/image (real-photo demo seed)

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,11 @@ const nextConfig: NextConfig = {
         hostname: "i.pravatar.cc",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**",
+      },
     ],
   },
   poweredByHeader: false,
